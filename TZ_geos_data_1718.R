aca57c4a6d31e499f9f80c5b6d7e545c74cd2481
mgwalsh <markusgwalsh@gmail.com>
2018-11-28 12:01:21 +0300

    initial commit

---
+++ TZ_geos_data_1718.R
@@ -0,0 +1,80 @@
+# Tanzania GeoSurvey 2017/2018 250m resolution data setup
+# M. Walsh, November 2018
+
+# Required packages
+# install.packages(c("downloader","rgdal","jsonlite","raster","leaflet","htmlwidgets","wordcloud")), dependencies=TRUE)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(rgdal)
+  require(jsonlite)
+  require(raster)
+  require(leaflet)
+  require(htmlwidgets)
+  require(wordcloud)
+})
+
+# Data downloads -----------------------------------------------------------
+# set working directory
+dir.create("TZ_1718", showWarnings = F)
+setwd("./TZ_1718")
+
+# download GeoSurvey data
+# see sampling frame @ https://github.com/mgwalsh/Sampling/blob/master/TZ_GS_sample.R
+download("https://www.dropbox.com/s/0x4y4j6ifqidmhh/TZ_geos_2018.csv.zip?raw=1", "TZ_geos_2018.csv.zip", mode = "wb")
+unzip("TZ_geos_2018.csv.zip", overwrite = T)
+geos <- read.table("TZ_geos_2018.csv", header = T, sep = ",")
+geos$BIC <- as.factor(ifelse(geos$CP == "Y" & geos$BP == "Y", "Y", "N")) ## identifies croplands with buildings
+
+# download GADM-L3 shapefile (courtesy: http://www.gadm.org)
+download("https://www.dropbox.com/s/bhefsc8u120uqwp/TZA_adm3.zip?raw=1", "TZA_adm3.zip", mode = "wb")
+unzip("TZA_adm3.zip", overwrite = T)
+shape <- shapefile("TZA_adm3.shp")
+
+# download raster stack (note this is a big 1+ Gb download)
+download("https://www.dropbox.com/s/pshrtvjf7navegu/TZ_250m_2017.zip?raw=1", "TZ_250m_2017.zip", mode = "wb")
+unzip("TZ_250m_2017.zip", overwrite = T)
+glist <- list.files(pattern="tif", full.names = T)
+grids <- stack(glist)
+
+# Data setup --------------------------------------------------------------
+# attach GADM-L3 admin unit names from shape
+coordinates(geos) <- ~lon+lat
+projection(geos) <- projection(shape)
+gadm <- geos %over% shape
+geos <- as.data.frame(geos)
+geos <- cbind(gadm[ ,c(5,7,9)], geos)
+colnames(geos) <- c("region","district","ward","survey","time","id","observer","lat","lon","BP","CP","WP","rice","bloc","cgrid","BIC")
+
+# project GeoSurvey coords to grid CRS
+geos.proj <- as.data.frame(project(cbind(geos$lon, geos$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(geos.proj) <- c("x","y")
+geos <- cbind(geos, geos.proj)
+coordinates(geos) <- ~x+y
+projection(geos) <- projection(grids)
+
+# extract gridded variables at GeoSurvey locations
+geosgrid <- extract(grids, geos)
+gsdat <- as.data.frame(cbind(geos, geosgrid)) 
+# gsdat <- gsdat[!duplicated(gsdat), ] ## removes any duplicates ... if needed
+gsdat <- gsdat[complete.cases(gsdat[ ,c(10:13,18:61)]),] ## removes incomplete cases
+gsdat <- gsdat[ which(gsdat$CP=='Y'), ] ## selects croplands only
+gsdat$observer <- sub("@.*", "", as.character(gsdat$observer)) ## shortens observer ID's
+
+# Write data frame --------------------------------------------------------
+dir.create("Results", showWarnings = F)
+write.csv(bcoord, "./Results/TZ_bcoord.csv", row.names = F)
+write.csv(gsdat, "./Results/TZ_gsdat.csv", row.names = F)
+
+# GeoSurvey map widget ----------------------------------------------------
+w <- leaflet() %>%
+  setView(lng = mean(gsdat$lon), lat = mean(gsdat$lat), zoom = 6) %>%
+  addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
+  addCircleMarkers(gsdat$lon, gsdat$lat, clusterOptions = markerClusterOptions())
+w ## plot widget 
+saveWidget(w, 'TZ_GS18.html', selfcontained = T) ## save widget
+
+# GeoSurvey contributions -------------------------------------------------
+gscon <- as.data.frame(table(gsdat$observer))
+set.seed(1235813)
+wordcloud(gscon$Var1, freq = gscon$Freq, scale = c(4,0.1), random.order = T)
+
