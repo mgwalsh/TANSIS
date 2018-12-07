ac5cbee557cc954922f7a3e6c04a87e703e717dc
mgwalsh <markusgwalsh@gmail.com>
2018-12-07 06:18:13 +0300

    Tanzania "Crop scout" data setup based on MobileSurvey ground observations

---
+++ TZ_crop_scout_data.R
@@ -0,0 +1,70 @@
+# Tanzania "Crop scout" data setup based on MobileSurvey ground observations
+# M. Walsh, December 2018
+
+# Required packages
+# install.packages(c("devtools","plyr","rgdal","raster","leaflet","htmlwidgets","wordcloud")), dependencies=TRUE)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(plyr)
+  require(rgdal)
+  require(raster)
+  require(leaflet)
+  require(htmlwidgets)
+  require(wordcloud)
+})
+
+# Data downloads -----------------------------------------------------------
+# set working directory
+dir.create("TZ_mobs", showWarnings = F)
+setwd("./TZ_mobs")
+
+# download MobileSurvey data
+download("https://www.dropbox.com/s/7tpbj22s8yjvxm9/TZ_crop_scout_2018.csv.zip?raw=1", "TZ_crop_scout_2018.csv.zip", mode = "wb")
+unzip("TZ_crop_scout_2018.csv.zip", overwrite = T)
+mobs <- read.table("TZ_crop_scout_2018.csv", header = T, sep = ",")
+
+# download GADM-L3 shapefile (courtesy: http://www.gadm.org)
+download("https://www.dropbox.com/s/bhefsc8u120uqwp/TZA_adm3.zip?raw=1", "TZA_adm3.zip", mode = "wb")
+unzip("TZA_adm3.zip", overwrite = T)
+shape <- shapefile("TZA_adm3.shp")
+
+# download raster stack (note this is a big 1+ Gb download)
+download("https://www.dropbox.com/s/pshrtvjf7navegu/TZ_250m_2017.zip?raw=1", "TZ_250m_2017.zip", mode = "wb")
+unzip("TZ_250m_2017.zip", overwrite = T)
+glist <- list.files(pattern="tif", full.names = T)
+grids <- stack(glist)
+
+# Data setup --------------------------------------------------------------
+# attach GADM-L3 admin unit names from shape
+coordinates(mobs) <- ~lon+lat
+projection(mobs) <- projection(shape)
+gadm <- mobs %over% shape
+mobs <- as.data.frame(mobs)
+mobs <- cbind(gadm[ ,c(5,7,9)], mobs)
+mobs <- rename(mobs, c("NAME_1"="region", "NAME_2"="district", "NAME_3"="ward"))
+
+# project GeoSurvey coords to grid CRS
+mobs.proj <- as.data.frame(project(cbind(mobs$lon, mobs$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(mobs.proj) <- c("x","y")
+mobs <- cbind(mobs, mobs.proj)
+coordinates(mobs) <- ~x+y
+projection(mobs) <- projection(grids)
+
+# extract gridded variables at GeoSurvey locations
+mobsgrid <- extract(grids, mobs)
+msdat <- as.data.frame(cbind(mobs, mobsgrid)) 
+# msdat <- msdat[!duplicated(msdat), ] ## removes any duplicates ... if needed
+# msdat <- msdat[complete.cases(msdat[ ,c(4:88)]),] ## removes incomplete cases
+# msdat <- msdat[ which(msdat$CP=='Y'), ] ## selects croplands only
+
+# Write data frame --------------------------------------------------------
+dir.create("Results", showWarnings = F)
+write.csv(msdat, "./Results/TZ_msdat_2018.csv", row.names = F)
+
+# GeoSurvey map widget ----------------------------------------------------
+w <- leaflet() %>%
+  setView(lng = mean(msdat$lon), lat = mean(msdat$lat), zoom = 6) %>%
+  addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
+  addCircleMarkers(msdat$lon, msdat$lat, clusterOptions = markerClusterOptions())
+w ## plot widget 
+saveWidget(w, 'TZ_mobs_2018.html', selfcontained = T) ## save widget
