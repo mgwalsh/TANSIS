1b9d867a2e382ab24851f71b6e21a8793008024e
mgwalsh <markusgwalsh@gmail.com>
2019-03-18 01:46:31 +0300

    Tanzania GeoSurvey 2017/2018 250m resolution data setup

--- TZ_geos_data_1718.R
+++ TZ_geos_data_1718.R
@@ -55,7 +55,7 @@ projection(geos) <- projection(grids)
 geosgrid <- extract(grids, geos)
 gsdat <- as.data.frame(cbind(geos, geosgrid)) 
 # gsdat <- gsdat[!duplicated(gsdat), ] ## removes any duplicates ... if needed
-gsdat <- gsdat[complete.cases(gsdat[ ,c(8:56)]),] ## removes incomplete cases
+gsdat <- gsdat[complete.cases(gsdat[ ,c(8:57)]),] ## removes incomplete cases
 # gsdat <- gsdat[ which(gsdat$CP=='Y'), ] ## selects croplands only
 gsdat$observer <- sub("@.*", "", as.character(gsdat$observer)) ## shortens observer ID's
 
