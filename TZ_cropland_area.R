3c15d07b0bf356c7a2958a10b82ddd2bbbae3fb2
mgwalsh <markusgwalsh@gmail.com>
2019-06-01 18:41:07 +0300

    Tanzania 2018 GeoSurvey 250m cropland area predictions

---
+++ TZ_cropland_area.R
@@ -0,0 +1,43 @@
+# Tanzania 2018 GeoSurvey 250m cropland area predictions
+# M. Walsh, June 2019
+
+# Required packages
+# install.packages(c("downloader","rgdal","raster","MASS","caret")), dependencies=TRUE)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(rgdal)
+  require(raster)
+  require(MASS)
+  require(caret)
+})
+
+# Data downloads -----------------------------------------------------------
+# set working directory
+dir.create("TZ_crop_area", showWarnings = F)
+setwd("./TZ_crop_area")
+
+# download GeoSurvey data
+# see sampling frame @ https://github.com/mgwalsh/Sampling/blob/master/TZ_GS_sample.R
+download("https://www.dropbox.com/s/64u27k4pk3j4vz1/TZ_cropland_area.csv.zip?raw=1", "TZ_cropland_area.csv.zip", mode = "wb")
+unzip("TZ_cropland_area.csv.zip", overwrite = T)
+geos <- read.table("TZ_cropland_area.csv", header = T, sep = ",")
+
+# download GADM-L3 shapefile (courtesy: http://www.gadm.org)
+download("https://www.dropbox.com/s/bhefsc8u120uqwp/TZA_adm3.zip?raw=1", "TZA_adm3.zip", mode = "wb")
+unzip("TZA_adm3.zip", overwrite = T)
+shape <- shapefile("TZA_adm3.shp")
+
+# attach GADM-L3 admin unit names from shape
+coordinates(geos) <- ~lon+lat
+projection(geos) <- projection(shape)
+gadm <- geos %over% shape
+geos <- as.data.frame(geos)
+geos <- cbind(gadm[ ,c(5,7,9)], geos)
+colnames(geos) <- c("region","district","ward","lat","lon","bp","cp","wp","bcount","ccount","cprob","cpred")
+
+# Models ------------------------------------------------------------------
+# negative binomial models
+summary(m1 <- glm.nb(ccount ~ cpred, geos))
+summary(m2 <- glm.nb(ccount ~ wp+cpred+bcount, geos))
+summary(m3 <- glm.nb(ccount ~ wp+cprob+bcount, geos))
+
