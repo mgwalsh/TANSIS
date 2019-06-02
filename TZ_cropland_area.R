d24b452e4227e34c3046fd9cb666b158879f36d9
mgwalsh <markusgwalsh@gmail.com>
2019-06-02 04:03:15 +0300

    Tanzania 2018 GeoSurvey 250m cropland area predictions

--- TZ_cropland_area.R
+++ TZ_cropland_area.R
@@ -2,13 +2,13 @@
 # M. Walsh, June 2019
 
 # Required packages
-# install.packages(c("downloader","rgdal","raster","MASS","caret")), dependencies=TRUE)
+# install.packages(c("downloader","rgdal","raster","MASS","ggplot2")), dependencies=TRUE)
 suppressPackageStartupMessages({
   require(downloader)
   require(rgdal)
   require(raster)
   require(MASS)
-  require(caret)
+  require(ggplot2)
 })
 
 # Data downloads -----------------------------------------------------------
@@ -37,6 +37,24 @@ colnames(geos) <- c("region","district","ward","lat","lon","bp","cp","wp","bcoun
 
 # Models ------------------------------------------------------------------
 # negative binomial models of GeoSurvey cropland grid counts
-summary(m1 <- glm.nb(ccount ~ cprob, geos))
-(est <- cbind(Estimate = coef(m1), confint(m1)))
+summary(m1 <- glm.nb(ccount ~ cprob, geos)) ## scaling model
+(est1 <- cbind(Estimate = coef(m1), confint(m1)))
 
+# +additional covariates
+summary(m2 <- glm.nb(ccount ~ cprob+bcount+wp, geos))
+(est2 <- cbind(Estimate = coef(m2), confint(m2)))
+anova(m1, m2) ## model comparison
+
+# Plots -------------------------------------------------------------------
+m1pred <- data.frame(cprob = rep(seq(from = 0.0, to = 1.0, length.out = 100)))
+m1pred <- cbind(m1pred, predict(m1, m1pred, type = "link", se.fit=TRUE))
+m1pred <- within(m1pred, {
+  ccount <- exp(fit)
+  LL <- exp(fit - 1.96 * se.fit)
+  UL <- exp(fit + 1.96 * se.fit)
+})
+
+ggplot(newdata2, aes(cprob, ccount)) +
+  geom_ribbon(aes(ymin = LL, ymax = UL, fill = prog), alpha = .25) +
+  geom_line(aes(colour = prog), size = 2) +
+  labs(x = "Math Score", y = "Predicted Days Absent")
