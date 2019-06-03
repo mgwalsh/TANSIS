e7e76f164b9d667575c740439f977abf442f1c27
mgwalsh <markusgwalsh@gmail.com>
2019-06-03 03:53:04 +0300

    Tanzania 2018 GeoSurvey 250m cropland area predictions

--- TZ_cropland_area.R
+++ TZ_cropland_area.R
@@ -8,7 +8,6 @@ suppressPackageStartupMessages({
   require(rgdal)
   require(raster)
   require(MASS)
-  require(ggplot2)
 })
 
 # Data downloads -----------------------------------------------------------
@@ -18,7 +17,7 @@ setwd("./TZ_crop_area")
 
 # download GeoSurvey data
 # see sampling frame @ https://github.com/mgwalsh/Sampling/blob/master/TZ_GS_sample.R
-download("https://www.dropbox.com/s/64u27k4pk3j4vz1/TZ_cropland_area.csv.zip?raw=1", "TZ_cropland_area.csv.zip", mode = "wb")
+download("https://www.dropbox.com/s/f4ucx0mv7jqn12u/TZ_cropland_area.csv.zip?raw=1", "TZ_cropland_area.csv.zip", mode = "wb")
 unzip("TZ_cropland_area.csv.zip", overwrite = T)
 geos <- read.table("TZ_cropland_area.csv", header = T, sep = ",")
 
@@ -27,7 +26,7 @@ download("https://www.dropbox.com/s/bhefsc8u120uqwp/TZA_adm3.zip?raw=1", "TZA_ad
 unzip("TZA_adm3.zip", overwrite = T)
 shape <- shapefile("TZA_adm3.shp")
 
-# download landcover raster masks
+# download landcover rasters
 download("https://www.dropbox.com/s/ijbsk94cx4wlb5f/TZ_GS_preds.zip?raw=1", "TZ_GS_preds.zip", mode = "wb")
 unzip("TZ_GS_preds.zip", overwrite = T)
 glist <- list.files(pattern="tif", full.names = T)
@@ -40,7 +39,7 @@ projection(geos) <- projection(shape)
 gadm <- geos %over% shape
 geos <- as.data.frame(geos)
 geos <- cbind(gadm[ ,c(5,7,9)], geos)
-colnames(geos) <- c("region","district","ward","lat","lon","bp","cp","wp","bcount","ccount","cprob","cpred")
+colnames(geos) <- c("region","district","ward","lat","lon","bp","cp","wp","bcount","ccount")
 
 # project GeoSurvey coords to grid CRS
 geos.proj <- as.data.frame(project(cbind(geos$lon, geos$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
@@ -53,36 +52,23 @@ projection(geos) <- projection(grids)
 geosgrid <- extract(grids, geos)
 gsdat <- as.data.frame(cbind(geos, geosgrid)) 
 # gsdat <- gsdat[!duplicated(gsdat), ] ## removes any duplicates ... if needed
-gsdat <- gsdat[complete.cases(gsdat[ ,c(8:57)]),] ## removes incomplete cases
-# gsdat <- gsdat[ which(gsdat$CP=='Y'), ] ## selects croplands only
-gsdat$observer <- sub("@.*", "", as.character(gsdat$observer)) ## shortens observer ID's
+gsdat <- gsdat[complete.cases(gsdat[ ,c(11:13)]),] ## removes incomplete cases
 
 # Write data frame --------------------------------------------------------
 dir.create("Results", showWarnings = F)
-write.csv(gsdat, "./Results/TZ_gsdat.csv", row.names = F)
+write.csv(gsdat, "./Results/TZ_crop_area.csv", row.names = F)
 
 # Models ------------------------------------------------------------------
 # negative binomial models of GeoSurvey cropland grid counts
-summary(m1 <- glm.nb(ccount ~ cpred, geos)) ## scaling model
+summary(m1 <- glm.nb(ccount ~ CP18, gsdat)) ## scaling model
 (est1 <- cbind(Estimate = coef(m1), confint(m1)))
-# geos$m1 <- predict(m1, geos)
+m1.pred <- predict(grids, m1, type="response")
+gsdat$m1 <- predict(m1, gsdat, type="response")
 
-# +additional covariates
-summary(m2 <- glm.nb(ccount ~ cpred+bcount+wp, geos))
+# +additional LCC covariates
+summary(m2 <- glm.nb(ccount ~ CP18+BP18+WP18, gsdat))
 (est2 <- cbind(Estimate = coef(m2), confint(m2)))
+m2.pred <- predict(grids, m2, type="response")
+gsdat$m2 <- predict(m2, gsdat, type="response")
 anova(m1, m2) ## model comparison
 
-# zero-inflated negative binomial
-
-# Plots -------------------------------------------------------------------
-m1pred <- data.frame(cprob = rep(seq(from = 0.0, to = 1.0, length.out = 100)))
-m1pred <- cbind(m1pred, predict(m1, m1pred, type = "link", se.fit=TRUE))
-m1pred <- within(m1pred, {
-  ccount <- exp(fit)
-  LL <- exp(fit - 1.96 * se.fit)
-  UL <- exp(fit + 1.96 * se.fit)
-})
-
-ggplot(m1pred, aes(cprob, ccount)) +
-  geom_ribbon(aes(ymin = LL, ymax = UL), alpha = .5) +
-  labs(x = "Cropland probability", y = "GeoSurvey cropland grid count")
