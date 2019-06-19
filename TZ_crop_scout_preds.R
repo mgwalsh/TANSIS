e44ccee5cf5437f9ebc147010fccb3149a2b18d6
mgwalsh <markusgwalsh@gmail.com>
2019-06-19 10:39:34 +0300

    Stacked predictions of Tanzania "Crop scout" observations

--- TZ_crop_scout_preds.R
+++ TZ_crop_scout_preds.R
@@ -234,7 +234,7 @@ plot(mask, axes=F)
 # Write prediction grids --------------------------------------------------
 gspreds <- stack(preds, 1-st.pred, mask)
 names(gspreds) <- c("gl1","gl2","rf","gb","nn","st","mk")
-writeRaster(gspreds, filename="./Results/TZ_maize_preds_2018.tif", datatype="FLT4S", options="INTERLEAVE=BAND", overwrite=T)
+writeRaster(gspreds, filename="./Results/TZ_maize_preds_2019.tif", datatype="FLT4S", options="INTERLEAVE=BAND", overwrite=T)
 
 # Write output data frame -------------------------------------------------
 coordinates(msdat) <- ~x+y
